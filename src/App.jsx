e324eabe2a30f3238c577561d69eaa3cfd08d8a3
Marisa <marisatriguerosperez@gmail.com>
2024-06-25 01:12:51 +0200

    Ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Clock from './components/Clock'
 import Counter from './components/Counter'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <h2> <Counter /> </h2>
+     <h2> <Clock /> </h2>
     </>
   )
 }
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment';
 
 function Clock() {
 
-    
+    const [currentTime, setCurrentTime] = useState(moment().format('HH:mm:ss'));
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(moment().format('HH:mm:ss'));
+        }, 1000);
+
+        // Evitar fugas de memoria, si no lo usamos se seguirá ejecutando 
+        // en segundo plano aunque el componente que lo creó ya no esté en el DOM
+        // Si no lo ponemos funciona igual, pero consumimos recursos innecesarios
+        return  () => clearInterval(timer);
+    })
+
   return (
-    <div>Clock</div>
+    <div>{currentTime} </div>
   )
 }
 
